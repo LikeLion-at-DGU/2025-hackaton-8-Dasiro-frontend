b950ca6c3c7e7ac4ca3a4d63522be958867fd10e
jjjung0921 <98745092+jjjung0921@users.noreply.github.com>
2025-08-13 02:55:03 +0900

    [Feat] 카카오 지도 불러오기 수정 중

--- src/pages/RecoveryPage.tsx
+++ src/pages/RecoveryPage.tsx
@@ -1,7 +1,10 @@
+import MapSeoulRisk from "@features/recovery-zone/MapSeoulRisk";
+
 const RecoveryPage = () => {
+  const appkey = import.meta.env.VITE_KAKAO_JS_KEY;
   return (
     <div style={{ padding: "40px", fontSize: "24px", fontWeight: "bold" }}>
-      회복상권 페이지입니다~
+      <MapSeoulRisk appKey={appkey} />
     </div>
   );
 };
